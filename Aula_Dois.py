279e542b7e09bc2a9a33197b53f374f538b7be1a
lorxnzy-baca <153696447+lorxnzy-baca@users.noreply.github.com>
2025-03-17 21:41:41 -0300

    Update README.md

---
+++ Aula_Dois.py
@@ -0,0 +1,67 @@
+# # print("Hello world!")
+# # print()
+
+# # # Exercicio_1
+# # print("Bruno")
+# # print()
+
+# # # Exercicio_2
+# # a = 3
+# # b = 5
+# # print(2*a*3*b)
+# # print()
+
+# # Exercicio_3
+# # num1 = 2
+# # num2 = 3
+# # num3 = 4
+# # soma = num1+num2+num3
+# # print(soma)
+# # Seleciona a linha e dê Crtl + ? para deixar "comentado" e pra tirar a mesma coisa.
+
+# #'''int( para inteiros'''
+# # num1 = int(input("Número 1 = "))
+# # num2 = int(input("Número 2 = "))
+# # num3 = int(input("Número 3 = "))
+# # soma = num1+num2+num3
+# # print(soma)
+
+# # '''float( para decimais'''
+# # num1 = float(input("Número 1 = "))
+# # num2 = float(input("Número 2 = "))
+# # num3 = float(input("Número 3 = "))
+# # soma = num1+num2+num3
+# # print(f"Soma = {soma:.2f}")
+# # print()
+
+# # '''Segunda aula'''
+#
+# # d = True
+# # print(type(d))
+
+# # nome = "Lorenzo Emanuel Bacaycoa"
+# # print(type(nome))
+#
+# # print(10**3)
+# # print(10//3)
+# # print(10%3)
+#
+# # nota = 8
+# # media = 7
+# # aprovado = nota >= media
+# # print(aprovado)
+#
+# # nota = float(input("Digite a nota do aluno: "))
+# # media = 6
+# # if (nota >= media):
+# #     print("Aprovado")
+# # else:
+# #     print("Reprovado")
+#
+# # nome_completo = input("Nome completo: ")
+# # idade = int(input("Idade: "))
+# # altura = float(input("Altura: "))
+#
+# # print("Seu nome é:",nome_completo)
+# # print("Sua idade é:",idade)
+# # print("Sua altura é:",altura)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Eng-computacao-Aulas-Phyton
+# Eng-computacao-Phyton
 Aulas sobre algoritmos em phytom para o curso de engenharia da computação
